90a85d0adeab1e0616975a4632646176279a130c
JoaoPedro205 <125528419+JoaoPedro205@users.noreply.github.com>
2023-08-22 20:37:52 -0300

    Add files via upload

---
+++ teste1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <string.h>
+main ()
+{
+char disciplina [30]= "Algoritmo e a mais legal!";
+printf ("\nDisciplina: %s",disciplina);
+}
+
+
