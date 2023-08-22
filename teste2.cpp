843701997bb603db0c30bbe458d4fd2741348bf5
JoaoPedro205 <125528419+JoaoPedro205@users.noreply.github.com>
2023-08-22 20:38:08 -0300

    Add files via upload

--- teste2.cpp
+++ teste2.cpp
@@ -1,53 +1,10 @@
 #include <stdio.h>
-
-#include <conio.h>
-
-#include <locale.h>
-
-#include <math.h>
-
+#include <string.h>
 main()
-
-{setlocale (LC_ALL,"Portuguese");
-
-int i,ano,idade,maior,soma;
-
-float media;
-
-maior=0;
-
-soma= 0;
-
-for (i=1;i<=10;i++)
-
-{printf ("\nDigite o ano em que voce nasceu: ");
-
-scanf ("%i",&ano);
-
-idade = 2023-ano;
-
-soma= soma+idade;
-
-
-
-if (idade >= 18)
-
-{printf ("\nVoce é maior de idade.");
-
-maior =maior + 1;
-
-}
-
-else {printf ("\nVoce é menor de idade.");
-
-}
-
-}
-
-media =soma/10;
-
-printf ("\nA media de idades é de %.2f",media);
-
-printf ("\nExistem %i maiores de idade",maior);
-
+{char nome[10];
+int tam;
+scanf ("%s",&nome);
+printf ("\nNome: %s",nome);
+tam= strlen(nome);
+printf ("\nTamanho: %i",tam);
 }
